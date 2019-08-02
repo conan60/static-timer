d41bd7098aea69f70a2b49ea8f91358ab6bc485c
conan60 <malek.gorchene@gmail.com>
2019-08-02 22:06:40 +0100

    Updates

---
+++ precache-manifest.7dda4eb67877ee6daeb3d4e97ff1e1f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea1175035b93df5f98152f4a26a917d",
+    "url": "/static-timer/index.html"
+  },
+  {
+    "revision": "6bcbaa0fe33c094e4e29",
+    "url": "/static-timer/static/css/main.8be634e1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static-timer/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "6bcbaa0fe33c094e4e29",
+    "url": "/static-timer/static/js/main.463a69f4.chunk.js"
+  },
+  {
+    "revision": "93e5ef52e10d7fbda620",
+    "url": "/static-timer/static/js/runtime~main.07e3a4d0.js"
+  }
+]);
